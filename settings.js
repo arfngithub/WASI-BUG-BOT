3aafcd93d1aad9fbe7cf1dd7b752d7742a959c2f
AR_FN <141509966+arfngithub@users.noreply.github.com>
2024-08-01 13:47:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923177835251"
+global.ownername = "🐛 MR AR "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
-global.location = "Pakistan, Lahore"
+global.location = "World 🌐"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '923177835251'  //creator number
+global.ownername = ' MR AR ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'AR'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "WASI TECH"
+global.wm = "AR TECH"
 
 //theme link
 global.link = 'https://whatsapp.com'
